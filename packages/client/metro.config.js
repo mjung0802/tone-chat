57a260926a6e2299360bad920d02c5e873977fc7
mjung0802 <daniel0802@gmail.com>
2026-03-01 20:00:38 -0700

    update metro configs to fix bad dependency causing react native to not boot up

--- packages/client/metro.config.js
+++ packages/client/metro.config.js
@@ -13,4 +13,19 @@ config.resolver.nodeModulesPaths = [
   path.resolve(monorepoRoot, 'node_modules'),
 ];
 
+// Force Metro to resolve pretty-format to v29 at root node_modules.
+// pnpm's .pnpm store contains a v30 copy that @expo/metro-runtime's
+// HMRClient picks up via symlinks, causing "prettyFormat.default is undefined".
+const prettyFormatPath = path.resolve(monorepoRoot, 'node_modules/pretty-format');
+const defaultResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'pretty-format') {
+    return { type: 'sourceFile', filePath: require.resolve('pretty-format', { paths: [prettyFormatPath] }) };
+  }
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
