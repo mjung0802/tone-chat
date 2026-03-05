af13059a2f9d899441699d7b5672e2ee84d81f1a
madaley1 <mor2daley@gmail.com>
2026-03-05 15:24:24 -0700

    wip/dockerContainer

--- packages/usersService/src/db/seed.ts
+++ packages/usersService/src/db/seed.ts
@@ -1,11 +1,20 @@
 import { client } from '@/client.ts';
 
+import createUsers from './001_create_users.sql';
+import createCredentials from './002_create_credentials.sql';
+import createRefreshTokens from './003_create_refresh_tokens.sql';
+
 (async () => {
   try {
     await client.connect();
     console.log('Connected to the database');
 
     // Your seed logic here
+    await client.query(createUsers);
+    await client.query(createCredentials);
+    await client.query(createRefreshTokens);
+
+    console.log('Database seeded successfully');
 
   } catch (err) {
     console.error('Error connecting to the database', err);
